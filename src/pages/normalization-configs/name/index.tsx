312d56f8f76fad78d7b0983449ba30d3b9f42d80
Savchenko Alexander <asavchenko@datatech.ru>
2024-07-17 13:34:32 +0700

    ref(pages): update normalizationConfig

--- src/pages/normalization-configs/name/index.tsx
+++ src/pages/normalization-configs/name/index.tsx
@@ -1,5 +1,6 @@
+import { SymbolIcon, TrashIcon } from '@radix-ui/react-icons'
 import { useCallback } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { safeParse } from 'valibot'
 import {
   Form,
@@ -7,13 +8,18 @@ import {
   fromFormValues,
   getByName,
   normalizationConfigSchema,
+  remove,
   toFormValues,
+  update,
 } from '~/entities/normalization-config'
 import { routes } from '~/shared/routes'
+import Button from '~/ui/button'
 import Container from '~/ui/container'
+import Flex from '~/ui/flex'
 import FForm, { toNestedErrors, useCreateForm } from '~/ui/form'
 import Heading from '~/ui/heading'
 import Section from '~/ui/section'
+import Spinner from '~/ui/spinner'
 
 export interface Props {
   className?: string | undefined
@@ -26,13 +32,13 @@ const displayName = 'page-NormalizationConfigs_name'
  */
 export default function Component(): JSX.Element {
   const { name = '' } = useParams<{ name: string }>()
+  const navigate = useNavigate()
 
   const form = useCreateForm<FormValues>(
     {
       onSubmit: (data) => {
         console.log('data', data)
-
-        // return void updateMutator.mutateAsync({ input: data })
+        updateMutator.mutate({ input: fromFormValues(form.getState().values) })
       },
       validate: (values) => {
         const normalizationConfig = fromFormValues(values)
@@ -44,7 +50,24 @@ export default function Component(): JSX.Element {
     { values: true },
   )
 
-  getByName.useCache(
+  const updateMutator = update.useCache({
+    onSuccess: (data) => {
+      // notify({ data: 'Сохранено', type: 'success' })
+      getByName.setCache({ name }, data.data)
+      form.initialize(toFormValues(data.data))
+    },
+    // onError: () => notify({ data: 'Ошибка', type: 'error' }),
+  })
+
+  const removeMutator = remove.useCache({
+    onSuccess: () => {
+      navigate(routes.normalizationConfigs.getURL())
+      // notify({ data: 'Сохранено', type: 'success' })
+    },
+    // onError: () => notify({ data: 'Ошибка', type: 'error' }),
+  })
+
+  const fetcher = getByName.useCache(
     { name },
     {
       onSuccess: (data) => form.initialize(toFormValues(data)),
@@ -56,16 +79,46 @@ export default function Component(): JSX.Element {
   return (
     <main className={displayName}>
       <Container p='1.5rem'>
-        <Section size='1'>
-          <Heading>{routes.normalizationConfigs.getName()}</Heading>
-        </Section>
-        <Section size='1'>
-          <FForm
-            form={form}
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            render={render}
-          />
-        </Section>
+        {fetcher.isLoading && (
+          <Flex width='100%' justify='center'>
+            <Spinner />
+          </Flex>
+        )}
+        {fetcher.isError && (
+          <Flex width='100%' justify='center' gap='2' align='center'>
+            Ошибка <Button onClick={() => fetcher.refetch()}>Перезагрузить</Button>
+          </Flex>
+        )}
+        {fetcher.isSuccess && (
+          <>
+            <Section size='1'>
+              <Heading>{routes.normalizationConfigs.getName()}</Heading>
+            </Section>
+            <Section size='1'>
+              <FForm
+                form={form}
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                render={render}
+              />
+            </Section>
+            <Flex gap='2' justify='between'>
+              <Button color='red' round={true} onClick={() => removeMutator.mutate({ id: fetcher.data.id })}>
+                <TrashIcon />
+              </Button>
+              <Flex gap='2'>
+                <Button variant='outline' onClick={() => form.reset()} disabled={!form.getState().dirty} round={true}>
+                  <SymbolIcon />
+                </Button>
+                <Button
+                  disabled={updateMutator.isLoading || !form.getState().dirty || form.getState().invalid}
+                  onClick={form.submit}
+                >
+                  Сохранить
+                </Button>
+              </Flex>
+            </Flex>
+          </>
+        )}
       </Container>
     </main>
   )
---
+++ src/ui/spinner/index.ts
@@ -0,0 +1,7 @@
+/**
+ * ui
+ */
+import { Spinner } from '@radix-ui/themes'
+
+export default Spinner
+export type { SpinnerProps } from '@radix-ui/themes'
