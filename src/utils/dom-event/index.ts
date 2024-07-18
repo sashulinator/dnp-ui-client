07899fbc9192bad3c15e088dbd77a8fcd3eaf0d4
Savchenko Alexander <asavchenko@datatech.ru>
2024-07-18 11:46:39 +0700

    feat(utils/dom-event): добавить

---
+++ src/utils/dom-event/index.ts
@@ -0,0 +1,11 @@
+/**
+ * is
+ */
+export { isMetaCtrlKey } from './is/meta-ctrl-key'
+
+/**
+ *
+ */
+export { keyListener } from './key-listener'
+export { setInputValue } from './set-input-value'
+export { observeResize } from './observe-resize'
