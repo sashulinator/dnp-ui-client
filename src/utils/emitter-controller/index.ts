3c24c063042ae79c7cd02e6593c22c3943bbfb0d
Savchenko Alexander <asavchenko@datatech.ru>
2024-07-18 13:08:34 +0700

    feat(utils/emitter-controller): добавить

---
+++ src/utils/emitter-controller/index.ts
@@ -0,0 +1,12 @@
+export { Emitter } from '../emitter'
+/**
+ * variant
+ */
+export { type DictionaryEvents, Dictionary } from './variants/dictionary'
+export { EmitterDictionary } from './variants/emitter-dictionary'
+
+/**
+ * widgets
+ */
+export { LastingProp, type LastingPropEvent } from './widgets/lasting-prop'
+export { Prop, type PropEvent } from './widgets/prop'
---
+++ src/utils/emitter-controller/variants/dictionary.ts
@@ -0,0 +1,56 @@
+import { Key, find, get } from '../../dictionary'
+import { Emitter } from '../../emitter'
+import { toDictionary } from '../../list'
+import { Id } from '~/utils/core'
+
+export type DictionaryEvents<TItem> = {
+  add: { item: TItem }
+  update: { item: TItem }
+  remove: { item: TItem }
+}
+
+/**
+ * Позволяет подписаться на события добвления/обновления/удаления
+ */
+export class Dictionary<TItem, E extends DictionaryEvents<TItem> = DictionaryEvents<TItem>> extends Emitter<E> {
+  items: Record<Key, TItem>
+
+  getId: (s: TItem) => Id
+
+  constructor(itemList: TItem[], getId: (s: TItem) => Id) {
+    super()
+
+    this.getId = getId
+
+    this.items = toDictionary(getId, itemList) || {}
+  }
+
+  values(): TItem[] {
+    return Object.values(this.items)
+  }
+
+  get = (id: Key | undefined): TItem => {
+    return get(this.items, id)
+  }
+
+  find = (id: Key | undefined): TItem | undefined => {
+    return find(this.items, id)
+  }
+
+  add = (item: TItem, event?: Record<string, unknown>): void => {
+    this.items[this.getId(item)] = item
+    this.emit('add', { item, ...event })
+  }
+
+  update = (item: TItem, event?: Record<string, unknown>): void => {
+    this.items[this.getId(item)] = item
+    this.emit('update', { item, ...event })
+  }
+
+  remove = (key: Key, event?: Record<string, unknown>): void => {
+    const item = this.get(key)
+    this.items[this.getId(item)] = item
+    delete this.items[this.getId(item)]
+    this.emit('remove', { item, ...event })
+  }
+}
