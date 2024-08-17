ab9d397685b18fc151a759e5d51a20d894288ce0
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-17 19:29:51 +0700

    refactor(operationalTable-Form): добавить nav в defaultValues DGR-0

--- src/entities/operational-table/ui/form/models/default-values.ts
+++ src/entities/operational-table/ui/form/models/default-values.ts
@@ -3,4 +3,5 @@ import { type CreateOperationalTable } from '../../../types/operational-table'
 export const defaultValues: Partial<CreateOperationalTable> = {
   kn: '',
   name: '',
+  nav: false,
 }
