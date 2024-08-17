3278de50cacb2e6a5dbced91690566a47ca12202
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-17 19:30:48 +0700

    refactor(process-Item): лэйаут DGR-0

--- src/entities/process/item/ui/item.tsx
+++ src/entities/process/item/ui/item.tsx
@@ -1,10 +1,11 @@
-import { Avatar } from '@radix-ui/themes'
 import dayjs from 'dayjs'
 import { Process } from '../../types/process'
 import './item.scss'
+import Avatar from '~/ui/avatar'
 import Badge from '~/ui/badge'
 import Card from '~/ui/card'
 import Flex from '~/ui/flex'
+import Separator from '~/ui/separator'
 import Text from '~/ui/text'
 import TextHighlighter from '~/ui/text-highlighter'
 import Tooltip from '~/ui/tooltip'
@@ -26,23 +27,24 @@ export default function Component(props: Props): JSX.Element {
   return (
     <Card key={item.id} asChild={false} className={c(displayName, className)}>
       <Flex>
-        <TextHighlighter size='4' tooltipContent='Название конфига нормализации'>
+        <TextHighlighter tooltipContent='Название конфига нормализации'>
           {item.normalizationConfig.name}
         </TextHighlighter>
       </Flex>
-      <Flex gap='4'>
+      <Flex gap='4' align='center'>
         <Flex align='center' gap='2'>
           {item.normalizationConfig.last ? <Badge color='green'>Текущий</Badge> : <Badge color='red'>Архивный</Badge>}
           <TextHighlighter color='yellow' tooltipContent='Версия конфига нормализации'>
             {item.normalizationConfig.v}
           </TextHighlighter>
         </Flex>
+        <Separator orientation='vertical' />
         <Flex gap='2' align='center'>
           <Tooltip content={item.createdBy.name}>
             <Avatar radius='full' size='1' fallback='a' src={item.createdBy.avatar} />
           </Tooltip>
           <Tooltip content='Создано'>
-            <Text color='gray' as='div' size='2'>
+            <Text color='gray' as='div' size='1'>
               {dayjs(item?.createdAt).format('DD.MM.YYYY HH:mm')}
             </Text>
           </Tooltip>
