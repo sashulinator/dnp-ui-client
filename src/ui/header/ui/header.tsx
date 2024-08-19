e424c0770245a375d567e9291ec2cb94daf153b8
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-19 11:00:16 +0700

    feat(ui-Header): добавить isProduction чекбокс DGR-0

--- src/ui/header/ui/header.tsx
+++ src/ui/header/ui/header.tsx
@@ -1,4 +1,7 @@
 import './header.scss'
+import Checkbox from '~/ui/checkbox'
+import Flex from '~/ui/flex'
+import TextHighlighter from '~/ui/text-highlighter'
 import { Switch } from '~/ui/theme'
 import { c } from '~/utils/core'
 
@@ -6,19 +9,44 @@ export interface Props {
   className?: string | undefined
 }
 
-const displayName = 'ui-Header'
+const NAME = 'ui-Header'
 
 /**
  * ui-Header
  */
 export default function Component(): JSX.Element {
+  const isProd = localStorage.getItem('env') === 'production'
+
   return (
-    <header className={c(displayName)}>
-      <div className='crumbs'></div>
-      <div className='settings'>
-        <Switch />
+    <header className={c(NAME)}>
+      <div className={`${NAME}_breadscrumbs`}>
+        {process.env.NODE_ENV === 'development' && (
+          <Flex justify='center' align='center' gap='m'>
+            <TextHighlighter color={isProd ? 'green' : 'red'}>
+              <label style={{ cursor: 'pointer', display: 'flex', alignItems: 'center', gap: '.5rem' }}>
+                production
+                <Checkbox
+                  variant='soft'
+                  defaultChecked={isProd}
+                  onCheckedChange={(value): void => {
+                    if (value) {
+                      localStorage.setItem('env', 'production')
+                    } else {
+                      localStorage.setItem('env', 'development')
+                    }
+                    location.reload()
+                  }}
+                />
+              </label>
+            </TextHighlighter>
+          </Flex>
+        )}
       </div>
+      <Flex className={`${NAME}_settings`} gap='4' align='center'>
+        <Switch />
+      </Flex>
     </header>
   )
 }
-Component.displayName = displayName
+
+Component.displayName = NAME
