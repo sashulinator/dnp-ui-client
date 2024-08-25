e34a58361fcb27bb0b74ec25e1b549462215b3a1
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-25 18:01:08 +0700

    feat(ui-Dialog): создать DGR-0

---
+++ src/ui/dialog/index.ts
@@ -0,0 +1,13 @@
+/**
+ * ui
+ */
+
+import { Dialog } from '@radix-ui/themes'
+
+export default Dialog
+
+/**
+ * props
+ */
+
+export type { RootProps as DialogProps } from '@radix-ui/themes/dist/esm/components/dialog.d.ts'
