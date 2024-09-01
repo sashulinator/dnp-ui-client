28f14a3d23cb22bf65b8bb0ba2d171f674b61a22
Savchenko Alexander <asavchenko@datatech.ru>
2024-09-01 21:04:23 +0700

    refactor(eslint): поправить DGR-0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   rules: {
+    '@typescript-eslint/consistent-type-imports': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/interface-name-prefix': 'off',
     'react-hooks/exhaustive-deps': [0],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-console': ['error'],
   },
 }
