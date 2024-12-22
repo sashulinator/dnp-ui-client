3ee14334ecd233182b14cff9026a31eab5f48ff6
Alexander Savchenko <asavchenko@datatech.ru>
2024-12-22 17:12:26 +0400

    feat(dcservice): findTables DGR-0

--- entities/database-container/dcservice/api.v1/find-tables.ts
+++ entities/database-container/dcservice/api.v1/find-tables.ts
@@ -1,11 +1,12 @@
 import { baseUrl } from './constants'
 
-export const NAME = 'find-databases'
+export const NAME = 'find-tables'
 
 export const url = `${baseUrl}/${NAME}`
 
 export type RequestParams = {
   id: string
+  database: string
 }
 
 export type Result = any
