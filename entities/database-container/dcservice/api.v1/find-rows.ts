9f1be941504463b45ce8f5168b91086996d4b48a
Alexander Savchenko <asavchenko@datatech.ru>
2024-12-24 21:46:43 +0400

    feat(dcsrvice): добавить getRows DGR-0

--- entities/database-container/dcservice/api.v1/find-rows.ts
+++ entities/database-container/dcservice/api.v1/find-rows.ts
@@ -1,21 +1,22 @@
-import type { Where } from '~/slices/where'
+import { type Where } from '~/slices/where'
 
-import type { Dctable } from '../../dctable'
 import { baseUrl } from './constants'
 
-export const NAME = 'find-tables'
+export const NAME = 'find-rows'
 
 export const url = `${baseUrl}/${NAME}`
 
 export type RequestParams = {
   id: string
   database: string
+  table: string
   where?: Where
   limit?: number
   offset?: number
 }
 
 export type Result = {
-  items: ({ name: string; display?: string } & Partial<Dctable>)[]
+  items: Record<string, unknown>[]
   total: number
+  columns: { name: string; display: string }[]
 }
