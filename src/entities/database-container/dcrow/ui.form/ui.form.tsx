908524a897084b77c458314018768a0a0bf6135e
Alexander Savchenko <asavchenko@datatech.ru>
2025-02-06 11:56:24 +0700

    feat(dcrow): добавить форму DGR-0

---
+++ src/entities/database-container/dcrow/ui.form/ui.form.tsx
@@ -0,0 +1,37 @@
+import Flex from '~/shared/flex'
+import { Field, StringField, type StringFieldProps } from '~/shared/form'
+import { c } from '~/utils/core'
+
+type Column = {
+  type: string
+  name: string
+  display: string
+}
+
+export interface Props {
+  className?: string | undefined
+  columns: Column[] | undefined
+}
+
+export const NAME = 'dnp-databaseContainer-dcrow-Form'
+
+export default function Component(props: Props): JSX.Element {
+  const { columns = [] } = props
+
+  return (
+    <Flex className={c(props.className, NAME)} direction={'column'} gap='4'>
+      {columns.map((item) => {
+        return (
+          <Field<string, StringFieldProps, HTMLInputElement>
+            component={StringField}
+            key={item.display}
+            name={item.name}
+            label={item.display}
+          />
+        )
+      })}
+    </Flex>
+  )
+}
+
+Component.displayName = NAME
