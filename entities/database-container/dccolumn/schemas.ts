9b58df92d35272e86e76df3fd1ddb1d3826215d8
Alexander Savchenko <asavchenko@datatech.ru>
2025-03-24 22:49:42 +0400

    fix(dccolumn): починить upsertByLocator и findRows DGR-0

--- entities/database-container/dccolumn/schemas.ts
+++ entities/database-container/dccolumn/schemas.ts
@@ -8,7 +8,6 @@ export const dccolumn = v.object({
   id: v.pipe(v.string(), v.nonEmpty()),
   name: v.pipe(v.string(), v.nonEmpty()),
   display: v.pipe(v.string(), v.nonEmpty()),
-  type: v.pipe(v.string(), v.nonEmpty()),
   dcserviceId: v.pipe(v.string(), v.nonEmpty()),
   database: v.pipe(v.string(), v.nonEmpty()),
   table: v.pipe(v.string(), v.nonEmpty()),
