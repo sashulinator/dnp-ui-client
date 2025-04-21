646a081c7b8dd8842f3056c6c5d6719951d64972
Alexander Savchenko <asavchenko@datatech.ru>
2025-04-21 10:57:45 +0700

    refactor(select-multiple-labeled): удалить DGR-0

--- src/shared/select-multiple/index.ts
+++ src/shared/select-multiple/index.ts
@@ -5,8 +5,3 @@ import { default as SelectMultiple } from './ui.select-multiple'
 
 export default SelectMultiple
 export { type Props as SelectMultipleProps, type Option } from './ui.select-multiple'
-
-/**
- * variant
- */
-export * as LabeledSelectMultiple from './v.labeled'
--- src/shared/select-multiple/v.labeled/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * ui
- */
-import { default as Labeled } from './ui.labeled'
-
-export default Labeled
-export { type Props as LabeledProps, type Option } from './ui.labeled'
