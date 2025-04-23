c77592515f3ab80e9fc2a8649d7f3620eb84616d
Alexander Savchenko <asavchenko@datatech.ru>
2025-04-23 22:43:07 +0700

    refactor(dcservice): удалить Item DGR-0

--- src/entities/database-container/dcservice/index.ts
+++ src/entities/database-container/dcservice/index.ts
@@ -1,7 +1,6 @@
 export * as api from './api.v1'
 
 export * as Form from './form'
-export * as Input from './input'
 export * as Item from './item'
 export * as TestConnection from './test-connection'
 
