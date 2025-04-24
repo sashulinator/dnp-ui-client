7ad6ff2bcb66df81a752905a1bc9d4b428928756
Alexander Savchenko <asavchenko@datatech.ru>
2025-04-24 13:33:40 +0700

    feat(dcservice): добавить picker DGR-0

--- entities/database-container/dcservice/api.v1/find-with-total.ts
+++ entities/database-container/dcservice/api.v1/find-with-total.ts
@@ -9,6 +9,8 @@ export type RequestParams = {
   skip?: number
   take?: number
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  sort?: any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   where?: any
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   orderBy?: any
